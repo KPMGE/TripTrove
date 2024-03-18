8d440966c61b93b001746dd7656382b15c5623c1
KPMGE <jesuskevin254@gmail.com>
2024-03-17 21:31:41 -0300

    :white_check_mark: ensure create holiday controller returns server error

--- backend/tests/presentation/controllers/create-holiday.spec.ts
+++ backend/tests/presentation/controllers/create-holiday.spec.ts
@@ -2,6 +2,15 @@ import { CreateHolidayController } from "../../../src/presentation/controllers/c
 import { HttpResponse } from "../../../src/presentation/protocols"
 import { StatusCodes } from 'http-status-codes'
 import { CreateHolidayServiceMock, ValidatorMock } from "../mocks"
+import { makeFakeParticipant } from "../../data/mocks/entities/participant"
+
+const makeFakeCreateHolidayRequest = () => ({
+  title: "any title",
+  description: "any description",
+  date: new Date(),
+  location: "any location",
+  participants: [makeFakeParticipant()]
+})
 
 describe('create-holiday-controller', () => {
   it('should return bad request if validator returns error', async () => {
@@ -11,15 +20,21 @@ describe('create-holiday-controller', () => {
 
     jest.spyOn(validatorMock, 'validate').mockReturnValueOnce(new Error('validation error'))
 
-    const response: HttpResponse = await sut.handle({
-      title: "any title",
-      description: "any description",
-      date: new Date(),
-      location: "any location",
-      participants: []
-    })
+    const response: HttpResponse = await sut.handle(makeFakeCreateHolidayRequest())
 
     expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST)
     expect(response.body).toEqual(new Error('validation error'))
+  })
+
+  it('should return server error if service throws', async () => {
+    const serviceMock = new CreateHolidayServiceMock()
+    const validatorMock = new ValidatorMock()
+    const sut = new CreateHolidayController(serviceMock, validatorMock)
+    serviceMock.execute = () => { throw new Error('service error') }
+
+    const response: HttpResponse = await sut.handle(makeFakeCreateHolidayRequest())
+
+    expect(response.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+    expect(response.body).toEqual(new Error('service error'))
   })
 })
